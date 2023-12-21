88555fdae039b28d803fe6f3668a1d13dc6711fb
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2023-12-22 03:19:18 +0600

    Imported Ball component, used useState hook, and added button which change numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+import Ball from './components/Ball/Ball';
+
 function App() {
-	return <div></div>;
+	const firstNumbers: number = [5, 11, 16, 23, 32];
+	const [number, setNumber] = useState<number[]>(firstNumbers);
+
+	const generateRandomNumbers = () => {
+		const randomNumbers: number[] = [];
+
+		while (randomNumbers.length < 5) {
+			const randomNumber: number = Math.floor(Math.random() * 32 + 5);
+			if (!randomNumbers.includes(randomNumber)) {
+				randomNumbers.push(randomNumber);
+			}
+		}
+		return randomNumbers.sort((a, b) => a - b);
+	};
+
+	const changeNumbers = () => {
+		const numbers = generateRandomNumbers();
+		setNumber(numbers);
+	};
+
+	return (
+		<div className='App'>
+			<Ball number={number[0]} />
+			<Ball number={number[1]} />
+			<Ball number={number[2]} />
+			<Ball number={number[3]} />
+			<Ball number={number[4]} />
+			<button onClick={changeNumbers}>Change numbers</button>
+		</div>
+	);
 }
 
 export default App;
